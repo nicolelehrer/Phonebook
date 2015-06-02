8ea1c99cc810e3aaf1d14334dfaa4a99ae646adf
Nicole Lehrer <nicole.lehrer@gmail.com>
2015-06-02 17:16:29 -0400

    added functionality to be run from command line - is a little roundabout isnce I am parsing input differently could add a way to switch between running in xcode where i have to parse a single string and running in terminal where args are alreadys egmented for me

--- PhoneBook/main.m
+++ PhoneBook/main.m
@@ -31,13 +31,13 @@ NSArray * segmentUserInputByStringCharSet(NSString * userInput, NSString * separ
         }
     }
     
-    /*
+    
      //need to explicitly use index to avoid enumerating while mutable array is being mutated
-     int i;
-     for (i=0; i<[usableComponents count]; i++){
-     NSLog(@"saved element %@", [usableComponents objectAtIndex:i]);
-     }
-     */
+//     int i;
+//     for (i=0; i<[usableStrings count]; i++){
+//     NSLog(@"saved element %@", [usableStrings objectAtIndex:i]);
+//     }
+//    NSLog(@"useablestrings count is %lu", [usableStrings count]);
     return usableStrings;
 }
 
@@ -59,14 +59,24 @@ void updatePlistWithFileNamePersonNumber(NSString * fileName, NSString * personN
         data = [[NSMutableDictionary alloc] initWithContentsOfFile: filePath];
         NSMutableArray * matches = [[NSMutableArray alloc] init];
         
+        //cases in which an existing key in dictionary is important
+        //add:error, change:execute, remove:execute, ^lookup:execute, ^reverse-lookup:execute
         for (NSString * key in [data allKeys]) {
-            if ([key isEqualToString:personName] && [commandName isEqualToString:@"add"]) { //checks for intentional change
+            if ([key isEqualToString:personName] && [commandName isEqualToString:@"add"]) {
                 NSLog(@"Person already exists - use change command to change phone number");
                 return;
             }
+            if ([key isEqualToString:personName] && [commandName isEqualToString:@"change"]) {
+                [data setObject:phoneNumber forKey:personName];
+                [data writeToFile:filePath atomically:YES];
+                NSLog(@"%@ : %@ in Phonebook called %@", personName, phoneNumber, fileName);
+                return;
+            }
+            
             if ([key isEqualToString:personName] && [commandName isEqualToString:@"remove"]) {
                 [data removeObjectForKey:personName];
                 [data writeToFile:filePath atomically:YES];
+                NSLog(@"Removed %@ : %@ in Phonebook called %@", personName, phoneNumber, fileName);
                 return;
             }
             
@@ -91,23 +101,31 @@ void updatePlistWithFileNamePersonNumber(NSString * fileName, NSString * personN
             else{
                 NSLog(@"No person found");
             }
+            return;
+        }
+        
+        //cases in which key is new
+        //add:execute, change:execute, remove:error, ^lookup:execute, ^reverse-lookup:execute
+        if (![commandName isEqualToString:@"remove"]) {
+            [data setObject:phoneNumber forKey:personName];
+            [data writeToFile:filePath atomically:YES];
+            NSLog(@"%@ : %@ in Phonebook called %@", personName, phoneNumber, fileName);
         }
         else{
-            [data setObject:phoneNumber forKey:personName];
+            NSLog(@"No person found to remove");
         }
     }
     else {
         
         if (![commandName isEqualToString:@"create"]) {
-            NSLog(@"You created a new PhoneBook called %@", fileName);
+            NSLog(@"Phonebook named %@ not found. You need to first create it", fileName);
         }
         else{
-            //don't want to lose data just added in previous case
-            //in this case making a new phonebook using a create
             data = [[NSMutableDictionary alloc] init];
+            NSLog(@"New phonebook named %@ created", fileName);
+            [data writeToFile:filePath atomically:YES];
         }
     }
-    [data writeToFile:filePath atomically:YES];
 }
 
 NSString * returnInputWithLastCharRemoved(NSString * input){
@@ -117,8 +135,6 @@ NSString * returnInputWithLastCharRemoved(NSString * input){
     }
     return nil;
 }
-
-
 
 
 void handleInput(NSString * input){
@@ -136,7 +152,6 @@ void handleInput(NSString * input){
     if ([commandName isEqualToString:@"create"]) { //check for args count
         if ([parsedBySpace count] == 2) {
             updatePlistWithFileNamePersonNumber(fileName, nil, nil, commandName);
-            NSLog(@"New phonebook named %@ created", fileName);
         }
         else{
             NSLog(@"Error: %@", numArgErrorMessage);
@@ -147,36 +162,52 @@ void handleInput(NSString * input){
     //others require parse by quote
     int numSingleQuotesFound = (int)[[input componentsSeparatedByString:@"'"] count] - 1;
     NSArray * parsedByQuote = segmentUserInputByStringCharSet(input, @"'");
+    int numArgs = (int)[parsedByQuote count];
+    NSLog(@"number of args is %i", numArgs);
     
     if (([commandName isEqualToString:@"add"] ||
          [commandName isEqualToString:@"change"])) {
         
-        if (numSingleQuotesFound == 4 && [parsedByQuote count] == 4) {
+        if (numSingleQuotesFound == 4 && numArgs == 4) {
             personName = [parsedByQuote objectAtIndex:1];
             phoneNumber = [parsedByQuote objectAtIndex:2];
             updatePlistWithFileNamePersonNumber(fileName, personName, phoneNumber, commandName);
-            NSLog(@"%@ : %@ in Phonebook called %@", personName, phoneNumber, fileName);
             return;
         }
-        else if(numSingleQuotesFound != 4){
-            NSLog(@"Error: %@", numSingQuoteErrorMessage);
+        else if(numArgs != 4){
+            NSLog(@"Error: %@", numArgErrorMessage);
         }
         else{
-            NSLog(@"Error: %@", numArgErrorMessage);
+            NSLog(@"Error: %@", numSingQuoteErrorMessage);
         }
         return;
     }
     
-    if (([commandName isEqualToString:@"remove"] || [commandName isEqualToString:@"lookup"] || [commandName isEqualToString:@"reverse-lookup"])) {
-        if (numSingleQuotesFound == 2 && [parsedByQuote count] == 3) {
-            phoneNumber = [parsedByQuote objectAtIndex:1];
-            updatePlistWithFileNamePersonNumber(fileName, nil, nil, commandName);
+    if ([commandName isEqualToString:@"remove"] || [commandName isEqualToString:@"lookup"]) {
+        if (numSingleQuotesFound == 2 && numArgs == 3) {
+            personName = [parsedByQuote objectAtIndex:1];
+            updatePlistWithFileNamePersonNumber(fileName, personName, phoneNumber, commandName);
+            return;
         }
-        else if(numSingleQuotesFound != 2){
-            NSLog(@"Error: %@", numSingQuoteErrorMessage);
+        else if(numArgs != 3){
+            NSLog(@"Error: %@", numArgErrorMessage);
         }
         else{
+            NSLog(@"Error: %@", numSingQuoteErrorMessage);
+        }
+        return;
+    }
+    
+    if ([commandName isEqualToString:@"reverse-lookup"]) {
+        if (numSingleQuotesFound == 2 && numArgs == 3) {
+            phoneNumber = [parsedByQuote objectAtIndex:1];
+            updatePlistWithFileNamePersonNumber(fileName, nil, phoneNumber, commandName);
+        }
+        else if(numArgs != 3){
             NSLog(@"Error: %@", numArgErrorMessage);
+        }
+        else{
+            NSLog(@"Error: %@", numSingQuoteErrorMessage);
         }
         return;
     }
@@ -185,19 +216,34 @@ void handleInput(NSString * input){
     
 }
 
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-    
-        while (true) {
-            
-            NSLog(@"Enter some data: ");
-            
-            char str[100] = {0}; // static allocation of string
-            fgets (str, sizeof(str), stdin); //input buffer, bufferlength, stin
+        
+//below is if you are taking user input from xCode command line
+//        NSLog(@"Enter some data: ");
+//        char str[100] = {0}; // static allocation of string
+//        fgets (str, sizeof(str), stdin); //input buffer, bufferlength, stin
+//        NSString * userInput = [NSString stringWithFormat:@"%s", str]; //convert c string to NSString
+        
+        NSString * arg1 = [NSString stringWithFormat:@"%s", argv[1]]; //command
+        NSString * arg2 = [NSString stringWithFormat:@"%s", argv[2]];
+        NSString * arg3 = [NSString stringWithFormat:@"%s", argv[3]];
+        NSString * arg4 = [NSString stringWithFormat:@"%s", argv[4]];
 
-            NSString * userInput = [NSString stringWithFormat:@"%s", str]; //convert c string to NSString
-            handleInput(userInput);
+        NSString * userInput;
+        if ([arg1 isEqualToString:@"create"]) {
+            userInput = [NSString stringWithFormat:@"%@ %@", arg1, arg2];
         }
+        else if ([arg1 isEqualToString:@"add"] || [arg1 isEqualToString:@"change"]) {
+            userInput = [NSString stringWithFormat:@"%@ %@ %@ %@", arg1, arg2, arg3, arg4];
+        }
+        else {
+            userInput = [NSString stringWithFormat:@"%@ %@ %@", arg1, arg2, arg3];
+        }
+
+        handleInput(userInput);
+
     }
     return 0;
 }
